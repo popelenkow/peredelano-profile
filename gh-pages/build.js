38d105c0051b4e34efa80f119f9ef9e76762490c
Vladlen Popelenkov <popelenkow@gmail.com>
2023-05-27 21:17:49 +0700

    Fix gh-pages build

--- gh-pages/build.js
+++ gh-pages/build.js
@@ -2,5 +2,5 @@ const copyfiles = require("copyfiles");
 
 const err = (error) => error && console.error(error);
 
-copyfiles(["node_modules/peredelano-profile/dist/*/", ""], 2, err);
+copyfiles(["node_modules/peredelano-profile/dist/**", ""], 2, err);
 copyfiles(["../.gitignore", "dist"], 1, err);
